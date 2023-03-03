b143b97680d7c762e5e7a115809df8c11bbeb4d9
emmanuelolofintuyi <EmmanuelOlofintuyi@gmail.com>
2023-03-03 13:47:59 -0500

    added new project for spring security

---
+++ .zshenv
@@ -0,0 +1,2 @@
+
+export PATH=${PATH}:/usr/local/mysql-8.0.31-macos12-arm64/bin
---
+++ emmanuel/src/test/java/com/workshop/emmanuel/EmmanuelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workshop.emmanuel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmmanuelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
